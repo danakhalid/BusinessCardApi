8a1113ec2de33c44a21ca4939a4cdd03616134a8
dana.khalidabdallah@gmail.com <dana.khalidabdallah@gmail.com>
2025-07-12 15:15:38 +0300

    add angualr config on prog.cs

--- BusinessCardApi/Program.cs
+++ BusinessCardApi/Program.cs
@@ -1,6 +1,7 @@
 using BusinessCardApi.DataDb;
 using BusinessCardApi.Repo;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,14 +17,36 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 builder.Services.AddScoped<IBusinessCardRepo, BusinessCardRepo>();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular",
+        policy => policy.WithOrigins("http://localhost:4200")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod());
+});
+
+
+
+
 var app = builder.Build();
 
+app.UseCors("AllowAngular");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseStaticFiles();
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(Directory.GetCurrentDirectory(), "Exports")),
+    RequestPath = "/Exports"
+});
+
 
 app.UseHttpsRedirection();
 
